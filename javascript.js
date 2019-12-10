019558272610a864a835cfbd5a95ace7898b0915
Phangster <mynamebryanphang@gmail.com>
2019-12-10 22:34:36 +0800

    change text

--- javascript.js
+++ javascript.js
@@ -1,7 +1,9 @@
 window.onload = function () {
     var btn = document.getElementById("btn");
     var iframe = document.getElementById("iframe");
+    var x = document.getElementById("myDIV");
     var srcs = ["https://editor.p5js.org/lewlian/embed/fM3QHCYT9", "https://editor.p5js.org/lewlian/embed/DczO27-hX"];
+    var header = ['Maurer Rose', 'Double Pendulum']
     var i = 0;
     btn.addEventListener("click", buttonClick, false);
 
@@ -9,6 +11,12 @@ window.onload = function () {
         iframe.src = srcs[i];
         console.log('loading src', srcs[i]);
         i = ((1 + i) % srcs.length);
+
+        if (x.innerHTML === "Maurer Rose") {
+          x.innerHTML = "Double Pendulum";
+        } else {
+          x.innerHTML = "Maurer Rose";
+        }
     };
 
     btn.click();
