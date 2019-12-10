2c5bcc3ee30bdbdfd0a79facec5f7def2a1ce7bd
Sean Lew <seanlewtengsiong@gmail.com>
2019-12-10 21:41:18 +0800

    updated my project and change title

--- javascript.js
+++ javascript.js
@@ -1,14 +1,14 @@
-window.onload=function(){
+window.onload = function () {
     var btn = document.getElementById("btn");
     var iframe = document.getElementById("iframe");
-    var srcs = ["https://editor.p5js.org/lewlian/embed/fM3QHCYT9", "https://editor.p5js.org/lewlian/embed/bU4jNrSUx"];
+    var srcs = ["https://editor.p5js.org/lewlian/embed/fM3QHCYT9", "https://editor.p5js.org/lewlian/embed/DczO27-hX"];
     var i = 0;
     btn.addEventListener("click", buttonClick, false);
 
     function buttonClick(e) {
-    iframe.src = srcs[i];
-    console.log('loading src', srcs[i]);
-    i = ((1 + i) % srcs.length);
+        iframe.src = srcs[i];
+        console.log('loading src', srcs[i]);
+        i = ((1 + i) % srcs.length);
     };
 
     btn.click();
